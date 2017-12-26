12adcdf36bedae771b3b38b677c264e5ee44107e
ydlmac2 <ydlmac2@ydlmac2deMacBook-Pro.local>
2017-12-26 14:58:26 +0800

    test push

---
+++ LTXMLYFM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LTXMLYFM
+//
+//  Created by ydlmac2 on 2017/12/26.
+//  Copyright © 2017年 ydlmac2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LTXMLYFM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LTXMLYFM
+//
+//  Created by ydlmac2 on 2017/12/26.
+//  Copyright © 2017年 ydlmac2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
